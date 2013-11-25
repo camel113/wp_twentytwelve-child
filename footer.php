cdb77e92a3d84c80b661b0981fe1409290238a03
Adrien Bigler <adrienbigler@MacBook-Pro-de-adrienbigler.local>
2013-11-25 21:42:18 +0100

    Add ajuster button on the bottom right

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
+			<span id="abg-responsive-resize"><a href="#"></a></span>
 			<span>&copy; <?php bloginfo( 'name' ); ?></span>
 			<span class="abg-r-footerinfo"><a href="http://www.pme-web.ch" title="Conception site internet simple pour PME et particuliers">pme-web.ch</span>
 		</div><!-- .site-info -->
