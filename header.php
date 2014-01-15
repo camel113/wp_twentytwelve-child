3318cb767f3592015a530d9d65380e34a4573b65
Adrien Bigler <adrienbigler@MacBook-Pro-de-adrienbigler.local>
2014-01-15 16:52:25 +0100

    Remove menu (navigation)

--- header.php
+++ header.php
@@ -55,11 +55,6 @@ if(navigator.userAgent.match(/Android/i)
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="main" class="wrapper">
