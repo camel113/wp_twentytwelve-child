d0bf1a3f7900bf98c5365e1e24bf3cc4842477f5
Adrien Bigler <adrienbigler@MacBook-Pro-de-adrienbigler.local>
2014-01-15 17:35:44 +0100

    Remove page and site title

--- header.php
+++ header.php
@@ -52,7 +52,7 @@ if(navigator.userAgent.match(/Android/i)
 		<?php endif; ?>
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<!-- <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2> -->
 		</hgroup>
 
 	</header><!-- #masthead -->
